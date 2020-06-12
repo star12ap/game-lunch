cdc21c1b8f7bd8026fd9715d2678f8219928c7e9
dwseo <dwseo@ibleaders.co.kr>
2020-06-12 16:36:07 +0900

    init menu

---
+++ game-lunch/src/components/Main.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+const style: React.CSSProperties = {
+  flex: 1,
+  overflow: 'auto',
+  border: 'solid 1px #ccc',
+  borderRadius: '4px',
+  backgroundColor:'white'
+}
+
+const Main: React.FC = (props) => {
+  return (
+    <main style={style}>
+      {props.children}
+    </main>
+  )
+}
+
+export default Main;
--- game-lunch/src/pages/Menu.js
+++ game-lunch/src/pages/Menu.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import Layout from '../components/Layout';
+import {Sidebar, SidebarTitle, SidebarBackButton} from '../components/Sidebar';
+import Main from '../components/Main';
+import { List, ListItem } from '../components/List';
+import { Link } from 'react-router-dom';
+import lists from '../data/Lists';
+
+const foods = lists.map((food) => {
+  return (
+    <ListItem>
+      <Link style={{ textDecoration: 'none' }} to="">{food.Name}</Link>
+    </ListItem>
+  )
+});
 
 class Menu extends React.Component {
   render() {
     return (
-      <div>
-        <section><div>test1</div></section>
-        <section><div>test2</div></section>
-      </div>
+      <Layout>
+      <Sidebar>
+        <SidebarBackButton to="/"></SidebarBackButton>
+        <List>
+          {foods}
+        </List>
+      </Sidebar>
+      <Main>
+        // 메뉴 생성 버튼
+        <Link to="/memo/add">새로운 메모</Link>
+      </Main>
+    </Layout>
     );
   }
 }
